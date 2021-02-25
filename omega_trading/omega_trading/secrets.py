cc99257290083b9d8f2b523a7b7fbab7b7fa6288
Caleb <crnorthc@eckerd.edu>
2021-02-25 12:51:10 -0500

    Hiding Secrets

---
+++ omega_trading/omega_trading/secrets.py
@@ -0,0 +1,1 @@
+Secret_Key = '7u)7cas%s7eotp2sft-e&%_2%ue$+pr^cf$p58uwy&(y2yx*j+'
--- omega_trading/omega_trading/settings.py
+++ omega_trading/omega_trading/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import Secret_Key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7u)7cas%s7eotp2sft-e&%_2%ue$+pr^cf$p58uwy&(y2yx*j+'
+SECRET_KEY = Secret_Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
