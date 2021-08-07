3b4cc73b9d67c242eed1163847bc4904b599d2c0
Matthew Klimek <matthewklimek@Matthews-Air.attlocal.net>
2021-08-07 15:08:42 -0400

    begining the proccess of adding tailwind

--- omega_trading/frontend/src/App.js
+++ omega_trading/frontend/src/App.js
@@ -1,41 +1,41 @@
-import Login from './components/layouts/Login.js'
-import SignUp from './components/layouts/SignUp.js'
-import VerifyAccount from './components/layouts/VerifyAccount.js'
-import ForgotPassword from './components/layouts/ForgotPassword.js'
-import ResetPassword from './components/layouts/ResetPassword.js'
-import Symbol from './components/layouts/Symbol.js'
-import MyNavbar from './components/layouts/MyNavbar.js'
-import Home from './components/layouts/Home.js'
-import Account from './components/layouts/Account.js'
-import Portfolio from './components/layouts/Portfolio.js'
-import Lobby from './components/layouts/Lobby.js'
-import Leaderboard from './components/layouts/Leaderboard.js'
-import React, { Fragment } from 'react'
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
-import './custom.scss'
-import './variables.scss'
+import Login from './components/layouts/Login.js';
+import SignUp from './components/layouts/SignUp.js';
+import VerifyAccount from './components/layouts/VerifyAccount.js';
+import ForgotPassword from './components/layouts/ForgotPassword.js';
+import ResetPassword from './components/layouts/ResetPassword.js';
+import Symbol from './components/layouts/Symbol.js';
+import MyNavbar from './components/layouts/MyNavbar.js';
+import Home from './components/layouts/Home.js';
+import Account from './components/layouts/Account.js';
+import Portfolio from './components/layouts/Portfolio.js';
+import Lobby from './components/layouts/Lobby.js';
+import Leaderboard from './components/layouts/Leaderboard.js';
+import React, { Fragment } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import './custom.scss';
+import './variables.scss';
 
 function App() {
-    return (
-        <Router>
-            <MyNavbar />
-            <Fragment>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/sign-up" component={SignUp} />
-                    <Route exact path="/verify-account" component={VerifyAccount} />
-                    <Route exact path="/forgot-password" component={ForgotPassword} />
-                    <Route exact path="/reset-password" component={ResetPassword} />
-                    <Route exact path="/chart" component={Symbol} />
-                    <Route exact path="/account" component={Account} />
-                    <Route exact path="/portfolio" component={Portfolio} />
-                    <Route exact path="/lobby" component={Lobby} />
-                    <Route exaxt path="/leaderboard" component={Leaderboard} />
-                </Switch>
-            </Fragment>
-        </Router>
-    )
+  return (
+    <Router>
+      <MyNavbar />
+      <Fragment>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/sign-up' component={SignUp} />
+          <Route exact path='/verify-account' component={VerifyAccount} />
+          <Route exact path='/forgot-password' component={ForgotPassword} />
+          <Route exact path='/reset-password' component={ResetPassword} />
+          <Route exact path='/chart' component={Symbol} />
+          <Route exact path='/account' component={Account} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/lobby' component={Lobby} />
+          <Route exaxt path='/leaderboard' component={Leaderboard} />
+        </Switch>
+      </Fragment>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- omega_trading/frontend/webpack.config.js
+++ omega_trading/frontend/webpack.config.js
@@ -1,51 +1,53 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "./static/frontend"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './static/frontend'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
         ],
-    },
-    optimization: {
-        minimize: true,
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                // This has effect on the react lib size
-                NODE_ENV: JSON.stringify("production"),
-            },
-        }),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      tailwindcss: {},
+      autoprefixer: {},
+      'process.env': {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
 };
