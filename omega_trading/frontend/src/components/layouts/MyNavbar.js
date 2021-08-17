0159afacbb9f9961e588c0d6e29481eaaa62a46b
Matthew <matthew.j.klimek@gmail.com>
2021-08-17 16:27:06 -0400

    before pull of main

--- omega_trading/frontend/src/components/layouts/MyNavbar.js
+++ omega_trading/frontend/src/components/layouts/MyNavbar.js
@@ -91,94 +91,102 @@ function MyNavbar(props) {
             <div className='flex items-center justify-center px-7 sm:px-5'>
               <Link
                 to={"/new-game"}
-                className='text-yellow-500 font-bold hover:text-gray-600'
+                className='text-yellow-500 font-bold hover:text-yellow-600'
               >
-                Cryptobet
+                Starbet
               </Link>
               <div className='w-40 sm:w-52 ml-5'>
                 <Listbox value={selected} onChange={setSelected}>
-                  <div className='rounded-lg shadow-md'>
-                    <div
-                      className={`
-                    ${selected === people[0] ? "bg-gray-900" : "bg-green-100 "} 
-                      relative flex items-center w-full py-2 pl-3 pr-10 text-left  rounded-lg  cursor-pointer focus:outline-none  sm:text-sm
+                  <div
+                    className={`
+                      relative 
                   `}
-                    >
-                      <a
-                        href='#fuck'
+                  >
+                    <div className='flex'>
+                      <div
                         className={`
                     ${
                       selected === people[0]
-                        ? "text-gray-200 hover:text-gray-300"
-                        : "text-green-900 hover:text-green-900 "
+                        ? "bg-gray-900 cursor-none"
+                        : "bg-gray-50 hover:bg-gray-300 cursor-pointer"
                     } 
-                      inline-block truncate
+                      flex-1 flex items-center py-2 pl-3 pr-10 text-left rounded-l-lg   focus:outline-none  sm:text-sm
                   `}
                       >
-                        {selected.name}
-                      </a>
-
-                      <Listbox.Button className=''>
-                        <span className='absolute inset-y-0 right-0 flex items-center pl-2 pr-2 cursor-pointer border-l-2 border-gray-400 bg-gray-900 rounded-r-lg'>
+                        <a
+                          href='#fuck'
+                          className={`
+                    ${
+                      selected === people[0]
+                        ? "text-gray-50 hover:text-gray-50 cursor-default"
+                        : "text-gray-800 hover:text-gray-900 cursor-pointer"
+                    }  
+                  `}
+                        >
+                          {selected.name}
+                        </a>
+                      </div>
+                      <Listbox.Button className='relative'>
+                        <span className='flex h-full items-center pl-2 pr-2 cursor-pointer border-l-2 border-white bg-gray-900 rounded-r-lg'>
                           <SelectorIcon
                             className='w-5 h-5 text-gray-400'
                             aria-hidden='true'
                           />
                         </span>
                       </Listbox.Button>
-                      <Transition
-                        as={Fragment}
-                        leave='transition ease-in duration-150'
-                        leaveFrom='opacity-100'
-                        leaveTo='opacity-0'
-                      >
-                        <Listbox.Options className='absolute top-10 right-0 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm '>
-                          {people.map((person, personIdx) => (
-                            <Listbox.Option
-                              key={personIdx}
-                              className={({ active }) =>
-                                `${
-                                  active && personIdx !== 0
-                                    ? "text-green-900 bg-green-100"
-                                    : active && personIdx === 0
-                                    ? "text-gray-800 bg-gray-200"
-                                    : "text-gray-900"
-                                }
-cursor-pointer select-none relative py-2 pl-10 pr-4`
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      leave='transition ease-in duration-150'
+                      leaveFrom='opacity-100'
+                      leaveTo='opacity-0'
+                    >
+                      <Listbox.Options className='absolute top-10 right-0 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm '>
+                        {people.map((person, personIdx) => (
+                          <Listbox.Option
+                            key={personIdx}
+                            className={({ active }) =>
+                              `${
+                                active && personIdx !== 0
+                                  ? "text-green-900 bg-green-100"
+                                  : active && personIdx === 0
+                                  ? "text-gray-800 bg-gray-200"
+                                  : "text-gray-900"
                               }
-                              value={person}
-                            >
-                              {({ selected, active }) => (
-                                <>
+cursor-pointer select-none relative py-2 pl-10 pr-4`
+                            }
+                            value={person}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <span
+                                  className={`${
+                                    selected ? "font-medium" : "font-normal"
+                                  } block truncate`}
+                                >
+                                  {person.name}
+                                </span>
+                                {selected ? (
                                   <span
                                     className={`${
-                                      selected ? "font-medium" : "font-normal"
-                                    } block truncate`}
-                                  >
-                                    {person.name}
-                                  </span>
-                                  {selected ? (
-                                    <span
-                                      className={`${
-                                        active
-                                          ? "text-amber-600"
-                                          : "text-amber-600"
-                                      }
+                                      active
+                                        ? "text-amber-600"
+                                        : "text-amber-600"
+                                    }
     absolute inset-y-0 left-0 flex items-center pl-3`}
-                                    >
-                                      <CheckIcon
-                                        className='w-5 h-5'
-                                        aria-hidden='true'
-                                      />
-                                    </span>
-                                  ) : null}
-                                </>
-                              )}
-                            </Listbox.Option>
-                          ))}
-                        </Listbox.Options>
-                      </Transition>
-                    </div>
+                                  >
+                                    <CheckIcon
+                                      className='w-5 h-5'
+                                      aria-hidden='true'
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
                   </div>
                 </Listbox>
               </div>
